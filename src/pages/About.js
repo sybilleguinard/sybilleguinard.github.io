67c8583fa6033f59068b53d725de165ce854f4e9
Pablo Guinard <guinardpablo@gmail.com>
2023-04-11 19:50:57 +0200

    change about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,7 +10,9 @@ function About() {
             <Header isSecondVisible />
             <div className="container about">
                 <img src={require('../uploads/about/' + data.about.avatar_path)} alt="Avatar" />
-                <div className="about-text">{data.about.desc}</div>
+                <div className="about-text">{data.about.desc.split('#').map((row, key) => {
+                    return row[0] === "g" ?  <b key={key}>{row.slice(1)}</b> : <p key={key}>{row.slice(1)}</p>
+                })}</div>
             </div>
             <Footer />
         </>
